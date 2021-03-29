65c2deff07255da9c1f6f5f3cbfd08ba5e419998
Andy Canul <ceandyemmanuel@gmail.com>
2021-03-28 20:04:41 -0600

    Agrega configuracion de Agenda

--- src/components/calendar/CalendarScreen.tsx
+++ src/components/calendar/CalendarScreen.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import { NavbarScreen } from '../ui/NavbarScreen'
+import React from "react";
+import { Calendar, Messages, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import { NavbarScreen } from "../ui/NavbarScreen";
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { messages } from "../../helpers/calendar_config";
+
+const localizer = momentLocalizer(moment);
+const myEventsList: [any] = [
+    {
+        title: "Hola", 
+        start: moment().toDate(), 
+        end: moment().toDate(), 
+        bgcolor: "#fafafa" 
+    }
+];
 
 export const CalendarScreen = () => {
-    return (
-        <div>
-            <NavbarScreen />
-        </div>
-    )
-}
+  return (
+    <div>
+      <NavbarScreen />
+      <Calendar
+        localizer={localizer}
+        events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+        messages={messages}
+      />
+    </div>
+  );
+};
